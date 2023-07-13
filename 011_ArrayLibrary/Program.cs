c25007763f50bb37a23ce178f1c3f5c47a738184
Alex <ansechin2012@ya.ru>
2023-07-13 12:41:51 +0300

    011_ArrayLibrary

---
+++ 011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿// взять массив из 10 рандомных элементов и распечатать их
+void FillArray(int[] collection) //FillArray заполнение массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //PrintArray печать массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // -1 вместо 0 чтобы показать в итоге если нет значения find в массиве 
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+// array[4] = 4; если нужно принудительно установить значение элементов в массиве
+// array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
