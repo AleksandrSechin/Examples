b2446d096efd05846a6853705e16ab3a5a08710d
Alex <ansechin2012@ya.ru>
2023-07-13 12:43:50 +0300

    010_MethodArray

---
+++ 010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// есть массив array из n элементов, требуется найти элемент массива равный find 
+
+int[] array = { 1, 12, 21, 45, 67, 18, 17, 45 };
+
+int n = array.Length;
+int find = 45;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //остановка поиска после первого найденног, если одинаковые значения элементов в массиве
+    }
+    index++; // index = index + 1
+}
+
+
