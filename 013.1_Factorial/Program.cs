6ca1bf96668fb533fd3ed21703278618ad8cfc1f
Alex <ansechin2012@ya.ru>
2023-07-21 16:13:58 +0300

    Факториал и числа Фибоначчи

---
+++ 013.1_Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿// Факториал
+
+int Factorial(int n)
+
+{
+    if (n == 1) return 1;  // 1! = 1   0! = 1
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(5));
+
+
+// Числа Фибоначчи - это последовательность чисел, где каждое число является суммой двух предыдущих чисел
+// f (1) = 1
+// f (2) = 1
+// f (n) = f (n-1) + f (n-2)
+
+int Fibonacci (int n){
+    if (n ==1 || n == 2) return 1;
+    else return Fibonacci (n-1) + Fibonacci (n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
